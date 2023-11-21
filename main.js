a0d2fba25a11812b9d13e8a02a46a80e139ef165
Thomas66 <thomassalvaterra2001@gmail.com>
2023-11-21 10:50:42 +0100

    bottone fixato

--- main.js
+++ main.js
@@ -8,14 +8,17 @@ createApp({
     },
     methods: {
         generaEmail() {
+
+            this.userEmail = [];
+
             for (let i = 0; i < 10; i++) {            
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(resp => {
-                console.log(resp.data.response);
-                let singleEmail = resp.data.response;
-                this.userEmail.push(singleEmail);
-                // document.getElementById("container").innerHTML  += `${this.userEmail}`
-            });
-            }
+                    let singleEmail = resp.data.response;
+                    if(this.userEmail.length < 10) {
+                        this.userEmail.push(singleEmail);
+                    }    
+                });
+            }           
         }
     },
     mounted() {      
