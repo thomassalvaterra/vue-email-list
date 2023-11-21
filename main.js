450090027efa582d79be10bf43438a7b23e781ce
Thomas66 <thomassalvaterra2001@gmail.com>
2023-11-21 10:13:07 +0100

    final version

--- main.js
+++ main.js
@@ -3,21 +3,21 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            userEmail: []
+            userEmail: [],
         }
     },
     methods: {
         generaEmail() {
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(resp => {
+            for (let i = 0; i < 10; i++) {            
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(resp => {
                 console.log(resp.data.response);
-                this.userEmail = resp.data.response;
-                console.log(this.userEmail);
+                let singleEmail = resp.data.response;
+                this.userEmail.push(singleEmail);
+                // document.getElementById("container").innerHTML  += `${this.userEmail}`
             });
+            }
         }
     },
-    mounted() {
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(resp => {
-            console.log("Ricevuto: ", resp.data);
-        });      
+    mounted() {      
     }
 }).mount('#app')
